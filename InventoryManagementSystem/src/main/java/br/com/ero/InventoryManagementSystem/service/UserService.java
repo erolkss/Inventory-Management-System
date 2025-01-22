9f3c390b1d46077540bbab15b307d8b0a769dd1e
Lucas Lima <lucaserolima@gmail.com>
2025-01-22 15:22:47 -0400

    feat: Added interface from methods to User

---
+++ InventoryManagementSystem/src/main/java/br/com/ero/InventoryManagementSystem/service/UserService.java
@@ -0,0 +1,25 @@
+package br.com.ero.InventoryManagementSystem.service;
+
+import br.com.ero.InventoryManagementSystem.dto.LoginRequest;
+import br.com.ero.InventoryManagementSystem.dto.RegisterRequest;
+import br.com.ero.InventoryManagementSystem.dto.Response;
+import br.com.ero.InventoryManagementSystem.dto.UserDTO;
+import br.com.ero.InventoryManagementSystem.entity.User;
+
+public interface UserService {
+
+    Response registerUser(RegisterRequest registerRequest);
+
+    Response loginUser(LoginRequest loginRequest);
+
+    User getAllUsers();
+
+    User getCurrentLoggeddInUser();
+
+    Response updateUser(Long id, UserDTO userDTO);
+
+    Response deleteUser(Long id);
+
+    Response getUserTransactions(Long id);
+
+}
