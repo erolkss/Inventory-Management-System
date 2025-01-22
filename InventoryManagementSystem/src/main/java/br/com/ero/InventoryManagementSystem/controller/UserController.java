3d6bc8d2bb530fb3ba63ad2bb8055b238906d870
Lucas Lima <lucaserolima@gmail.com>
2025-01-22 17:23:32 -0400

    feat(UserController): Created a new Controller from user and a handler method to get all users

---
+++ InventoryManagementSystem/src/main/java/br/com/ero/InventoryManagementSystem/controller/UserController.java
@@ -0,0 +1,26 @@
+package br.com.ero.InventoryManagementSystem.controller;
+
+import br.com.ero.InventoryManagementSystem.dto.LoginRequest;
+import br.com.ero.InventoryManagementSystem.dto.RegisterRequest;
+import br.com.ero.InventoryManagementSystem.dto.Response;
+import br.com.ero.InventoryManagementSystem.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+}
