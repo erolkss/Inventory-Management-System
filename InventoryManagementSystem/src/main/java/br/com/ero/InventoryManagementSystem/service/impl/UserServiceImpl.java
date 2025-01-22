616d4c040278d468ced0e6505356de791d8fd57c
Lucas Lima <lucaserolima@gmail.com>
2025-01-22 16:06:44 -0400

    feat(UserServiceImpl): Implemented method to get current logged in User

--- InventoryManagementSystem/src/main/java/br/com/ero/InventoryManagementSystem/service/impl/UserServiceImpl.java
+++ InventoryManagementSystem/src/main/java/br/com/ero/InventoryManagementSystem/service/impl/UserServiceImpl.java
@@ -16,6 +16,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -93,7 +95,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getCurrentLoggeddInUser() {
-        return null;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new NotFoundException("Email Not Found"));
+
+        user.setTransactions(null);
+
+        return user;
     }
 
     @Override
