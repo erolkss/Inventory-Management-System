48cbe6f666c0749dfdf3256306f0c0440892e873
Lucas Lima <lucaserolima@gmail.com>
2025-01-23 17:15:25 -0400

    feat: add addNewSupplier method in SupplierController

---
+++ InventoryManagementSystem/src/main/java/br/com/ero/InventoryManagementSystem/controller/SupplierController.java
@@ -0,0 +1,25 @@
+package br.com.ero.InventoryManagementSystem.controller;
+
+import br.com.ero.InventoryManagementSystem.dto.Response;
+import br.com.ero.InventoryManagementSystem.dto.SupplierDTO;
+import br.com.ero.InventoryManagementSystem.service.SupplierService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/suppliers")
+@RequiredArgsConstructor
+public class SupplierController {
+
+    private final SupplierService supplierService;
+
+    @PostMapping("/add")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> addNewSupplier(@RequestBody @Valid SupplierDTO supplierDTO) {
+        return ResponseEntity.ok(supplierService.addSupplier(supplierDTO));
+    }
+
+}
